9512f4c7bf3908f4d5fd12512e6517f825eb34f7
Joel Shepherd <joeldamole0@gmail.com>
2023-09-09 22:55:00 +1200

    More files missed in refactor

--- catkin_ws/devel/share/test_get_image/cmake/test_get_imageConfig.cmake
+++ catkin_ws/devel/share/test_get_image/cmake/test_get_imageConfig.cmake
@@ -67,14 +67,14 @@ set(test_get_image_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(test_get_image_SOURCE_PREFIX /home/joelshep/documents/university/COMPX582/camera/catkin_ws/src/test_get_image)
-  set(test_get_image_DEVEL_PREFIX /home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel)
+  set(test_get_image_SOURCE_PREFIX /home/joelshep/documents/university/ENGEN582/catkin_ws/src/test_get_image)
+  set(test_get_image_DEVEL_PREFIX /home/joelshep/documents/university/ENGEN582/catkin_ws/devel)
   set(test_get_image_INSTALL_PREFIX "")
   set(test_get_image_PREFIX ${test_get_image_DEVEL_PREFIX})
 else()
   set(test_get_image_SOURCE_PREFIX "")
   set(test_get_image_DEVEL_PREFIX "")
-  set(test_get_image_INSTALL_PREFIX /home/joelshep/documents/university/COMPX582/camera/catkin_ws/install)
+  set(test_get_image_INSTALL_PREFIX /home/joelshep/documents/university/ENGEN582/catkin_ws/install)
   set(test_get_image_PREFIX ${test_get_image_INSTALL_PREFIX})
 endif()
 
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(test_get_image_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel/include " STREQUAL " ")
+if(NOT "/home/joelshep/documents/university/ENGEN582/catkin_ws/devel/include " STREQUAL " ")
   set(test_get_image_INCLUDE_DIRS "")
-  set(_include_dirs "/home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel/include")
+  set(_include_dirs "/home/joelshep/documents/university/ENGEN582/catkin_ws/devel/include")
   if(NOT " " STREQUAL " ")
     set(_report "Check the issue tracker '' and consider creating a ticket if the problem has not been reported yet.")
   elseif(NOT " " STREQUAL " ")
@@ -110,7 +110,7 @@ if(NOT "/home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel/incl
         message(FATAL_ERROR "Project 'test_get_image' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'test_get_image' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/joelshep/documents/university/COMPX582/camera/catkin_ws/src/test_get_image/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'test_get_image' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/joelshep/documents/university/ENGEN582/catkin_ws/src/test_get_image/${idir}'.  ${_report}")
     endif()
     _list_append_unique(test_get_image_INCLUDE_DIRS ${include})
   endforeach()
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel/lib;/home/joelshep/documents/university/COMPX582/camera/catkin_ws/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/joelshep/documents/university/ENGEN582/catkin_ws/devel/lib;/home/joelshep/documents/university/ENGEN582/catkin_ws/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
