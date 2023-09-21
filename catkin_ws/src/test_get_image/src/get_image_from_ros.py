a53d9a02fc2d00c75c097c1732157b59815cef18
Joel Shepherd <joeldamole0@gmail.com>
2023-09-21 22:04:21 +1200

    Tried inverting matrices

--- catkin_ws/src/test_get_image/src/get_image_from_ros.py
+++ catkin_ws/src/test_get_image/src/get_image_from_ros.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 from math import sqrt
+from typing import Optional
 from blessed import Terminal # type: ignore
 
 import os
 import threading
 import numpy as np
 import yaml
+from camera import camera
 import rospy
-from test_get_image.msg import ImageBundle # type: ignore
 import queue
 import cv2 as cv
 
@@ -19,6 +20,8 @@ window_name = "Camera Test"
 
 image_queue = queue.Queue(2)
 
+term: Optional[Terminal] = None
+
 def ReceiveImages(image):
     try:
         image_queue.put_nowait(image)
@@ -26,7 +29,167 @@ def ReceiveImages(image):
         # Don't store old images
         pass
 
-def ProcessImages(det: Detector, term=None, stop_event=None):
+
+def obtain_image(image):
+    Images = image.Images
+    # print(f"Images headers {image.header}")
+    # print(f"Images LLH {image.LLH}")
+    # print(f"Images Device IDs {image.DeviceIDs}")
+    img = Images[0]
+    
+    # print(img.data)
+    # print(f"Array Sizes: Expected: {img.height*img.width*3} | Actual: {len(img.data)}")
+    img_array_data = np.frombuffer(img.data, dtype=np.uint8)
+    
+    img_array = np.array(img_array_data, dtype=np.uint8).reshape(img.height, img.width, 3)
+
+    # Convert the image array to uint8 type (expected by OpenCV)
+    img_uint8 = np.uint8(img_array)
+    img_flipped = cv.rotate(img_uint8, cv.ROTATE_180) # type: ignore
+    # print(f'Width: {img.width} | Height: {img.height} | Data Count: {len(img.data)} | Encoding: {img.encoding} | Shape: {img_flipped.shape}')
+    return img_flipped
+
+# def show_stats(term, x_rd, z_rd, x_ad, z_ad, x, z, angle, rot_calced, area, other_calced):
+def show_stats(term, x, z, area):
+    if term is None:
+        print("Can't show stats, no display")
+        return
+    # Center the status page
+    with term.location(0, term.height // 2 - 5):
+        # Display right/left distance (90 degrees to forward)
+        # print(term.center(f"Right/Left Distance: {x:.3f} m ({x_adjusted:.3f})"))
+        # print(term.center(f"Right/Left Distance: {x_ad:.3f} m ({x:.3f})"))
+        print(term.center(f"Right/Left Distance: {x:.3f} m"))
+        # x_scaled = x_rd*2
+        x_scaled = -int(round(x))*2
+        # Create a horizontal line of "-" for the forward distance
+        print(term.center(" "*x_scaled + "—" * abs(x_scaled) + " "*-x_scaled))
+        
+        # Create vertical lines of "|" for the right/left distance
+        # for _ in range(z_rd):
+        for _ in range(abs(int(round(z)))):
+            print(term.center("|"))
+        # print(term.center(f"Forward Distance: {z:.3f} m ({z_adjusted:.3f})"))
+        # print(term.center(f"Forward Distance: {z_ad:.3f} m ({z:.3f})"))
+        print(term.center(f"Forward Distance: {z:.3f} m"))
+        # print(term.center(f"Angle (degs): {angle: .2f}°"))
+        print(term.center(f"Area: {area: .2f}"))
+        # print(term.center(f"Angles (degs): X: {rot_x: .2f}° Y: {rot_y: .2f}° Z: {rot_z: .2f}°"))
+        # print(term.center(f"Angles\n{rot_calced[0]}\n{rot_calced[1]}\n{rot_calced[2]}\n"))
+        # print(term.center(f"Angles X\n{other_calced['x'][0]}\n{other_calced['x'][1]}"))
+        # print(term.center(f"Angles Y\n{other_calced['y'][0]}\n{other_calced['y'][1]}"))
+        # print(term.center(f"Angles Z\n{other_calced['z'][0]}\n{other_calced['z'][1]}"))
+        # print(term.center(f"Average X {other_calced['avg']['x']:.2f} Y {other_calced['avg']['y']:.2f} Z {other_calced['avg']['z']:.2f}"))
+
+def calc_pose_opencv(tag, det, image, term=None):
+    
+    if tag.pose_t is not None:
+        if det.calibrator is not None:
+            draw_vectors(tag, det, image)
+            
+        t_vec, R_vec = invert_matrices(tag.pose_t, tag.pose_R)
+        
+        # x = tag.pose_t[0][-1]/100
+        # y = tag.pose_t[1][-1]/100
+        # z = tag.pose_t[2][-1]/100
+        
+        x = t_vec[0][-1]/100
+        y = t_vec[1][-1]/100
+        z = t_vec[2][-1]/100
+        
+        dist = sqrt(x**2 + y**2 + z**2)
+        
+        # # Extract the first row (or first column) of the rotation matrix
+        # camera_orientation = tag.pose_R[0, :]
+
+        # # Calculate the angle left or right (in radians) relative to the camera's orientation
+        # angle_left_right = np.arccos(camera_orientation[0])
+        
+        # rot_calced = [[f"{np.degrees(np.arccos(ang)): .2f}°" for ang in row] for row in tag.pose_R]
+        # other_calced = {
+        #     "x": [
+        #         [np.degrees(np.arccos(tag.pose_R[1, 1])), np.degrees(-1*np.arcsin(tag.pose_R[1, 2]))],
+        #         [np.degrees(np.arcsin(tag.pose_R[2, 1])), np.degrees(np.arccos(tag.pose_R[2, 2]))]
+        #     ],
+        #     "y": [
+        #         [np.degrees(np.arccos(tag.pose_R[0, 0])), np.degrees(np.arcsin(tag.pose_R[0, 2]))],
+        #         [np.degrees(-1*np.arcsin(tag.pose_R[2, 0])), np.degrees(np.arccos(tag.pose_R[2, 2]))]
+        #     ],
+        #     "z": [
+        #         [np.degrees(np.arccos(tag.pose_R[0, 0])), np.degrees(-1*np.arcsin(tag.pose_R[0, 1]))],
+        #         [np.degrees(np.arcsin(tag.pose_R[1, 0])), np.degrees(np.arccos(tag.pose_R[1, 1]))]
+        #     ]
+        # }
+        
+        # avg_other_calced = {
+        #     "x": (abs(other_calced['x'][0][0]) + abs(other_calced['x'][0][1]) + abs(other_calced['x'][1][0]) + abs(other_calced['x'][1][1]))/4,
+        #     "y": (abs(other_calced['y'][0][0]) + abs(other_calced['y'][0][1]) + abs(other_calced['y'][1][0]) + abs(other_calced['y'][1][1]))/4,
+        #     "z": (abs(other_calced['z'][0][0]) + abs(other_calced['z'][0][1]) + abs(other_calced['z'][1][0]) + abs(other_calced['z'][1][1]))/4,
+        # }
+        
+        # other_calced['avg'] = avg_other_calced
+        
+        # angle_left_right_degrees =  np.degrees(angle_left_right)
+
+        # x_adjusted = z * np.sin(angle_left_right)
+        # z_adjusted = z * np.cos(angle_left_right)
+        # x_adjusted, z_adjusted = calculate_adjustment(z, x, angle_left_right)
+        # # x_rounded = int(round(x))
+        # x_rounded = int(round(x_adjusted))
+        # y_rounded = int(round(y))
+        # # z_rounded = int(round(z))
+        # z_rounded = int(round(z_adjusted))
+        area = calc_area(tag.corners)
+        # show_stats(term, x_rounded, z_rounded, x_adjusted, z_adjusted, x, z, angle_left_right_degrees, rot_calced, area, other_calced)
+        show_stats(term, x, z, area)
+    else:
+        print("Can't calculate pose")
+        
+
+def calc_area(c):
+    x1 = c[0, 0]
+    x2 = c[1, 0]
+    x3 = c[2, 0]
+    x4 = c[3, 0]
+    y1 = c[0, 1]
+    y2 = c[1, 1]
+    y3 = c[2, 1]
+    y4 = c[3, 1]
+    
+    # From https://www.themathdoctors.org/polygon-coordinates-and-areas/#:~:text=If%20the%20points%20are%20(x1,right%20and%20divide%20by%202.)
+    area = abs(0.5 * ((x1*y2-x2*y1) + (x2*y3-x3*y2) + (x3*y4-x4*y3) + (x4*y1-x1*y4)))
+    return area
+
+def calc_pose_tag(tag, image):
+    pass
+
+def invert_matrices(t, R):
+    R_inv = np.linalg.inv(R)
+    t_inv = -np.dot(R_inv, t)
+    return t_inv, R_inv
+
+def draw_vectors(tag, det, img):
+    # Draw the vectors
+    axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3) # type: ignore
+
+    imgpts, jac = cv.projectPoints(axis, tag.pose_R, tag.pose_t, det.calibrator.camera_matrix, det.calibrator.dist)
+
+    def draw(img, corners, imgpts):
+        def to_int(x):
+            return int(x)
+        
+        corner = tuple(corners[0].ravel())
+        corner = tuple(map(lambda p: int(p), corner))
+        print(f"Point to print: {corner}")
+        img = cv.line(img, corner, tuple(map(to_int, imgpts[2].ravel())), (0,0,255), 5)
+        img = cv.line(img, corner, tuple(map(to_int, imgpts[0].ravel())), (255,0,0), 5)
+        img = cv.line(img, corner, tuple(map(to_int, imgpts[1].ravel())), (0,255,0), 5)
+        return img
+        
+    overlayed = draw(img,tag.corners,imgpts)
+    return overlayed
+
+def ProcessImages(det: Detector, term=None, stop_event=None, ros=True):
     if not stop_event:
         stop_event = threading.Event()
     
@@ -37,31 +200,19 @@ def ProcessImages(det: Detector, term=None, stop_event=None):
             continue
         term = term
         detector: Detector = det
-        
-        Images = image.Images
-        # print(f"Images headers {image.header}")
-        # print(f"Images LLH {image.LLH}")
-        # print(f"Images Device IDs {image.DeviceIDs}")
-        img = Images[0]
-        
-        # print(img.data)
-        # print(f"Array Sizes: Expected: {img.height*img.width*3} | Actual: {len(img.data)}")
-        img_array_data = np.frombuffer(img.data, dtype=np.uint8)
-        
-        img_array = np.array(img_array_data, dtype=np.uint8).reshape(img.height, img.width, 3)
-
-        # Convert the image array to uint8 type (expected by OpenCV)
-        img_uint8 = np.uint8(img_array)
-        img_flipped = cv.rotate(img_uint8, cv.ROTATE_180) # type: ignore
-        # print(f'Width: {img.width} | Height: {img.height} | Data Count: {len(img.data)} | Encoding: {img.encoding} | Shape: {img_flipped.shape}')
+        if ros:
+            img_flipped = obtain_image(image)
+            bw_image = cv.cvtColor(img_flipped, cv.COLOR_BGR2GRAY)
+        else:
+            img_flipped = image
+            bw_image = image
         
         # calibration = Calibrator()
-        
-        bw_image = cv.cvtColor(img_flipped, cv .COLOR_BGR2GRAY)
         tags = detector.detect(bw_image)
         # for tag in tags:
         #     print(f"Tag found, ID: {tag.tag_id} | type: {type(tag.tag_id)}")
-        overlayed = detector.overlay_tags(img_flipped, tags, input_color=True)
+        
+        overlayed = detector.overlay_tags(img_flipped, tags, input_color=ros)
         
         if term is not None:
             print(term.clear())
@@ -71,82 +222,20 @@ def ProcessImages(det: Detector, term=None, stop_event=None):
                 if len(tags) == 0:
                     print("No Tags Found")
                 else:
-                    tag = tags[0]
-                    if tag.pose_t is not None:
-                        if det.calibrator is not None:
-                            # Draw the vectors
-                            axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3) # type: ignore
-            
-                            imgpts, jac = cv.projectPoints(axis, tag.pose_R, tag.pose_t, det.calibrator.camera_matrix, det.calibrator.dist)
-
-                            def draw(img, corners, imgpts):
-                                def to_int(x):
-                                    return int(x)
-                                
-                                corner = tuple(corners[0].ravel())
-                                corner = tuple(map(lambda p: int(p), corner))
-                                print(f"Point to print: {corner}")
-                                img = cv.line(img, corner, tuple(map(to_int, imgpts[2].ravel())), (0,0,255), 5)
-                                img = cv.line(img, corner, tuple(map(to_int, imgpts[0].ravel())), (255,0,0), 5)
-                                img = cv.line(img, corner, tuple(map(to_int, imgpts[1].ravel())), (0,255,0), 5)
-                                return img
-                                
-                            overlayed = draw(overlayed,tag.corners,imgpts)
-                            
-                        x = tag.pose_t[0][-1]/100
-                        y = tag.pose_t[1][-1]/100
-                        z = tag.pose_t[2][-1]/100
-                        dist = sqrt(x**2 + y**2 + z**2)
-                        
-                        # Extract the first row (or first column) of the rotation matrix
-                        camera_orientation = tag.pose_R[0, :]
-
-                        # Calculate the angle left or right (in radians) relative to the camera's orientation
-                        angle_left_right = np.arccos(camera_orientation[0])
-                        
-                        rot_calced = [[f"{np.degrees(np.arccos(ang)): .2f}°" for ang in row] for row in tag.pose_R]
-                        
-                        
-                        angle_left_right_degrees =  np.degrees(angle_left_right)
-
-                        # x_adjusted = z * np.sin(angle_left_right)
-                        # z_adjusted = z * np.cos(angle_left_right)
-                        x_adjusted, z_adjusted = calculate_adjustment(z, x, angle_left_right)
-                        # x_rounded = int(round(x))
-                        x_rounded = int(round(x_adjusted))
-                        y_rounded = int(round(y))
-                        # z_rounded = int(round(z))
-                        z_rounded = int(round(z_adjusted))
-                        
-                        # Center the status page
-                        with term.location(0, term.height // 2 - 5):
-                            # Display right/left distance (90 degrees to forward)
-                            # print(term.center(f"Right/Left Distance: {x:.3f} m ({x_adjusted:.3f})"))
-                            print(term.center(f"Right/Left Distance: {x_adjusted:.3f} m ({x:.3f})"))
-                            
-                            x_scaled = x_rounded*2
-                            # Create a horizontal line of "-" for the forward distance
-                            print(term.center(" "*x_scaled + "—" * abs(x_scaled) + " "*-x_scaled))
-                            
-                            # Create vertical lines of "|" for the right/left distance
-                            for _ in range(z_rounded):
-                                print(term.center("|"))
-                            # print(term.center(f"Forward Distance: {z:.3f} m ({z_adjusted:.3f})"))
-                            print(term.center(f"Forward Distance: {z_adjusted:.3f} m ({z:.3f})"))
-                            print(term.center(f"Angle (degs): {angle_left_right_degrees: .2f}°"))
-                            # print(term.center(f"Angles (degs): X: {rot_x: .2f}° Y: {rot_y: .2f}° Z: {rot_z: .2f}°"))
-                            print(term.center(f"Angles\n{rot_calced[0]}\n{rot_calced[1]}\n{rot_calced[2]}\n"))
+                    calc_pose_opencv(tags[0], det, overlayed, term)
+                    
                     for tag in tags:
                         if tag.pose_t is not None:
                             x = tag.pose_t[0][-1]
                             y = tag.pose_t[1][-1]
                             z = tag.pose_t[2][-1]
                             dist = sqrt(x**2 + y**2 + z**2)
-                            print(f"\rTime: {image.header.stamp.secs}, ID: {tag.tag_id}, {f'Distance: F {z: .2f} R {x: .2f} Abs {dist.real: .2f}cm' if dist is not None else ''}{' '*20}", end="")
+                            print(f"\rTime: {image.header.stamp.secs if ros else 'None'}, ID: {tag.tag_id}, {f'Distance: F {z: .2f} R {x: .2f} Abs {dist.real: .2f}cm' if dist is not None else ''}{' '*20}", end="")
                             # print(f"ID: {tag.tag_id}, Distance: {dist}")
+                    
         else:
             if len(tags) == 0:
-                print(f"\rNo Tags Found, Time: {image.header.stamp.secs}" + " "*40, end="")
+                print(f"\rNo Tags Found, Time: {image.header.stamp.secs if ros else 'None'}" + " "*40, end="")
             for tag in tags:
                 if tag.pose_t is not None:
                     x = tag.pose_t[0][-1]
@@ -161,10 +250,11 @@ def ProcessImages(det: Detector, term=None, stop_event=None):
 
                     # Convert the angle from radians to degrees
                     angle_left_right_degrees = np.degrees(angle_left_right)
-                    print(f"\rTime: {image.header.stamp.secs}, ID: {tag.tag_id}, {f'Distance: F {z: .2f} R {x: .2f} Abs {dist.real: .2f}cm' if dist is not None else ''}{' '*20} Angle {angle_left_right_degrees}°", end="")
+                    print(f"\rTime: {image.header.stamp.secs if ros else 'None'}, ID: {tag.tag_id}, {f'Distance: F {z: .2f} R {x: .2f} Abs {dist.real: .2f}cm' if dist is not None else ''}{' '*20} Angle {angle_left_right_degrees}°", end="")
                     # print(f"ID: {tag.tag_id}, Distance: {dist}")
                 else:
-                    print(f"\rTime: {image.header.stamp.secs}, ID: {tag.tag_id}", end="")
+                    print("No pose estimated")
+                    print(f"\rTime: {image.header.stamp.secs if ros else 'None'}, ID: {tag.tag_id}", end="")
         cv.namedWindow(window_name, cv.WINDOW_NORMAL)
         cv.imshow(window_name, overlayed)
         # cv.imshow(window_name, bw_image)
@@ -188,7 +278,8 @@ def calculate_adjustment(a, b, angle) -> tuple:
     x = p+q
     return x, y
     
-if __name__ == '__main__':
+def run_ros():
+    from test_get_image.msg import ImageBundle # type: ignore
     rospy.init_node("test_get_camera", disable_signals=True)
 
     print(f"CWD {os.getcwd()}")
@@ -223,3 +314,47 @@ if __name__ == '__main__':
     rospy.signal_shutdown("Closing")
     
     cv.destroyAllWindows()
+
+
+def continuous_camera_capture(event: threading.Event):
+    cam = camera.Camera(color=False, auto=False)
+    while not event.is_set():
+        try:
+            image_queue.put_nowait(cam.get_image())
+        except queue.Full:
+            # Don't store old images
+            pass
+    cam.close_camera()
+
+def run_local():
+    print("Loading camera config")
+    camera_config_file = './src/test_get_image/CameraConfigs/basler_1L.yaml'
+    with open(camera_config_file, 'rt') as file:
+        camera_config = yaml.safe_load(file)
+    cam_matrix = camera_config["camera_matrix"]["data"]
+    camera_matrix = np.array(camera_config['camera_matrix']['data']).reshape(3, 3)
+    dist_coeffs = np.array(camera_config['distortion_coefficients']['data'])
+    
+    calibration = Calibrator(camera_matrix, dist=dist_coeffs)
+    detector = Detector(14, calibrator=calibration)
+    term = Terminal()
+    # term = None
+    with term.fullscreen(), term.cbreak():        
+        event = threading.Event()
+        print("Starting processing")
+        process_thread = threading.Thread(target=ProcessImages, args=(detector, term, event, False))
+        process_thread.start()
+        print("Starting Capture")
+        capture_thread = threading.Thread(target=continuous_camera_capture, args=(event,))
+        capture_thread.start()
+        try:
+            event.wait()
+        except KeyboardInterrupt:
+            event.set()
+    print("Closing")
+    process_thread.join()
+    capture_thread.join()
+
+if __name__ == '__main__':
+    run_ros()
+    # run_local()
